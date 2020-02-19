c3ffe39c47f3be5af887fa1b652fb982a7633f8f
Shaun Thomas <shaunthomas999@gmail.com>
2020-02-19 13:36:08 +0100

    More commits

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# My spring-boot playground application
+
+## Technologies used
+
+* Java 8
+* Maven 3.6.3
+* Spring-boot 2.2.0
--- src/test/java/com/shaunthomas999/springplayground/SpringPlaygroundApplicationTests.java
+++ src/test/java/com/shaunthomas999/springplayground/SpringPlaygroundApplicationTests.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SpringPlaygroundApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+    assertTrue(true);
+  }
 }
